30a82a6ca5a36725de60e271f05b22d4c0a12eaa
number-25 <swnt@protonmail.com>
2025-04-04 13:47:16 +1000

    basic updates

--- src/MotifScanner.jl
+++ src/MotifScanner.jl
@@ -6,3 +6,22 @@ using FASTX
 # Write your package code here.
 
 end
+
+
+
+
+
+# Typos to push 
+
+
+# SamplerWeighted 
+Weighted sampler of type T. Instantiate with a collection of eltype T containing the
+  elements to sample, and an **orderen** collection of probabilities to sample each element
+  except the last. The last probability is the remaining probability up to 1.
+
+
+# src/biosequence/predicates.hl 
+!!! note
+    Using the [`reverse_complement`](@ref) of a DNA sequence will **give give** this
+    reverse complement. 
+
